edbb35102ab20d708e2416540928132f993adacc
oscar <oscantillomen@gmail.com>
2020-02-20 17:58:52 -0500

    Create custom hooks for selects

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from './useDropdown';
 
 const SearchParams = () => {
-    const location = "Seattle, WA";
+	const [ location, setLocation ] = useState('Seattle, WA');
+    const [ breeds, setBreeds ] = useState([]);
+    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
 
-    return (
-        <div className="search-params">
-            <form>
-                <label htmlFor="location">
-                    Location
-                    <input 
-                        type="location" 
-                        value={location}
-                        placeholder="Location"
-                    />
-                </label>
-                <button>Submit</button>
-            </form>
-        </div>
-    )
-}
+	return (
+		<div className="search-params">
+			<form>
+				<label htmlFor="location">
+					Location
+					<input
+						type="location"
+						value={location}
+						onChange={(e) => setLocation(e.target.value)}
+						placeholder="Location"
+					/>
+				</label>
+				<AnimalDropdown />
+                <BreedDropdown />
+				<button>Submit</button>
+			</form>
+		</div>
+	);
+};
 
 export default SearchParams;
