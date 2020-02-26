c9b5eca87880c23bcc19225f6bed3f5b32f3924d
oscantillomen <oscantillomen@gmail.com>
2020-02-26 13:33:25 -0500

    get pets from api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,17 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
+import Results from './Results';
 
 const SearchParams = () => {
-	const [ location, setLocation ] = useState('Seattle, WA');
-    const [ breeds, setBreeds ] = useState([]);
-    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+	const [location, setLocation] = useState('Seattle, WA');
+	const [breeds, setBreeds] = useState([]);
+	const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
 	const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
-	
+	const [pets, setPets] = useState([]);
+
+	async function requestPets() {
+		const { animals } = await pet.animals({
+		  location,
+		  breed,
+		  type: animal
+		});
+	  
+		setPets(animals || []);
+	  }
+
+	async function requestPets(){
+		const data = await pet.animals({
+			location,
+			breed,
+			type: animal
+		})
+		console.log(data)
+	}
+
 	useEffect(() => {
 		setBreeds([]);
 		setBreed("");
-		
+
 		pet.breeds(animal).then(({ breeds: apiBreeds }) => {
 			const breedStrings = apiBreeds.map(({ name }) => name);
 			setBreeds(breedStrings);
@@ -20,7 +41,10 @@ const SearchParams = () => {
 
 	return (
 		<div className="search-params">
-			<form>
+			<form onSubmit={e => {
+				e.preventDefault();
+				requestPets();
+			}}>
 				<label htmlFor="location">
 					Location
 					<input
@@ -31,9 +55,10 @@ const SearchParams = () => {
 					/>
 				</label>
 				<AnimalDropdown />
-                <BreedDropdown />
+				<BreedDropdown />
 				<button>Submit</button>
 			</form>
+			<Results pets={pets} />
 		</div>
 	);
 };
